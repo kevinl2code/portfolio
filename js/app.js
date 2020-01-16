996e45df8a76cc731d5490e343b5e2ad271b842a
Kevin Smith <kevinl2code@gmail.com>
2020-01-15 22:39:11 -0600

    Init commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+window.onscroll = () => toggleNav()
+
+const navigation = document.querySelector(".navigation")
+const navbar = document.querySelector("#navbar")
+const about = document.querySelector("#about")
+const navTop = about.offsetTop
+
+const toggleNav = () => {
+    if (window.pageYOffset >= navTop) {
+        navbar.classList.add("sticky")
+        navigation.setAttribute("style", "visibility: visible")
+    } else {
+        navbar.classList.remove("sticky")
+        navigation.setAttribute("style", "visibility: hidden")
+    }
+}
+
+function stickyNavigation() { 
+    console.log('navTop = ' + navTop);
+    console.log('scrollY = ' + window.scrollY);
+}
+
+window.addEventListener('scroll', stickyNavigation)
+
+console.log(navbar.offsetParent)
+console.log('hi')
